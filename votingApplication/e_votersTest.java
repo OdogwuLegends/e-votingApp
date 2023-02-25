2268e7642404b0b4af533333483e0b1a5fc1cdf7
OdogwuLegends <119644231+OdogwuLegends@users.noreply.github.com>
2023-02-25 12:16:42 -0800

    Add files via upload
    first commit

---
+++ votingApplication/e_votersTest.java
@@ -0,0 +1,29 @@
+//package eVotingApp;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//class e_votingTest {
+//    private Address address;
+//    private VotersDetails votersDetails;
+//    private PvcDetails pvcDetails;
+//    private e_voters voters;
+//
+//    @BeforeEach
+//    void setUp(){
+//        address = new Address();
+//        votersDetails = new VotersDetails();
+//        pvcDetails = new PvcDetails();
+//        voters = new e_voters();
+//    }
+//
+//    @Test
+//    void testThatVoterCanVote(){
+//       e_voters voters1 = voters.ableToVote();
+//       assertNotNull(voters1);
+//    }
+//
+//
+//}
